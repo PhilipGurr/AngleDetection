b88d0e33c763b10dd90cdb4feebc6b6fdee4f063
Philip Gurr <philipgurr10@gmail.com>
2022-04-20 00:29:05 +0200

    Initial commit

---
+++ AForgeAngleDetection/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace AForgeAngleDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelAngle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::AForgeAngleDetection.Properties.Resources.triangle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1364, 680);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(465, 856);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(468, 42);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelAngle
+            // 
+            this.labelAngle.AutoSize = true;
+            this.labelAngle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAngle.Location = new System.Drawing.Point(619, 758);
+            this.labelAngle.Name = "labelAngle";
+            this.labelAngle.Size = new System.Drawing.Size(155, 42);
+            this.labelAngle.TabIndex = 2;
+            this.labelAngle.Text = "Angle: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 920);
+            this.Controls.Add(this.labelAngle);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelAngle;
+    }
+}
+
---
+++ AForgeAngleDetection/Form1.cs
@@ -0,0 +1,106 @@
+﻿using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AForgeAngleDetection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Assembly assembly = this.GetType().Assembly;
+            Bitmap image = Properties.Resources.triangle;
+
+            ProcessImage(image);
+        }
+
+        private void ProcessImage(Bitmap bitmap)
+        {
+            BitmapData bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            ColorFiltering colorFilter = new ColorFiltering();
+
+            colorFilter.Red = new IntRange(240, 260);
+            colorFilter.Green = new IntRange(240, 260);
+            colorFilter.Blue = new IntRange(240, 260);
+            colorFilter.FillOutsideRange = false;
+
+            colorFilter.ApplyInPlace(bitmapData);
+
+            BlobCounter blobCounter = new BlobCounter();
+
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage(bitmapData);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            bitmap.UnlockBits(bitmapData);
+
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+
+            Graphics g = Graphics.FromImage(bitmap);
+            Pen bluePen = new Pen(Color.Blue, 5);
+            foreach (Blob blob in blobs)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blob);
+
+                List<IntPoint> corners;
+                if(shapeChecker.IsTriangle(edgePoints, out corners))
+                {
+                    var subType = shapeChecker.CheckPolygonSubType(corners);
+                    Console.WriteLine(subType);
+                    foreach(var corner in corners)
+                    {
+                        Console.WriteLine("X: " + corner.X + " Y: " + corner.Y);
+                    }
+                    g.DrawPolygon(bluePen, ToPointsArray(corners));
+
+                    var point1 = corners[1];
+                    var point2 = corners[2];
+
+                    Line verticalLine = Line.FromPoints(point1, point2);
+                    Line horizontalLine = Line.FromPoints(corners[0], corners[2]);
+                    double aforgeAngle = verticalLine.GetAngleBetweenLines(horizontalLine);
+                    //double angle = Math.Atan2(point2.Y - point1.Y, point2.X - point1.X) / Math.PI * 180.0; //ols
+                    labelAngle.Text = "Angle: " + aforgeAngle;
+                }
+            }
+
+            pictureBox.Image = bitmap;
+            bluePen.Dispose();
+            g.Dispose();
+        }
+
+        private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            System.Drawing.Point[] array = new System.Drawing.Point[points.Count];
+
+            for (int i = 0, n = points.Count; i < n; i++)
+            {
+                array[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+
+            return array;
+        }
+    }
+}
